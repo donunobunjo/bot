c2684df8471b5d8bd789618b37d4a15fe7a2c8b4
donunobunjo <don.unobunjo@gmail.com>
2023-02-05 12:47:00 +0100

    setup sdk

---
+++ app/Api/V1/Controllers/BotController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+class BotController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function test(){
+        dd("Test");
+    }
+    //
+}
---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BotController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+
+    public function test(){
+        dd('test');
+    }
+}
